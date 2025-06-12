6fc497acf9ee9274a14009451ee9155d77bc965e
3amry <74096502+3amry@users.noreply.github.com>
2025-06-12 08:17:23 +0300

    Running the idea of a console market.

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,20 @@
+import java.util.Scanner;
+
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        Scanner scanner = new Scanner(System.in);
+        String[] landingPageOptions = {"Sign in", "Sign up", "Continue to market"};
+        String[] landingPageRoutes = {"Signing in...",
+                "Signing up...", "Continuing to market..."};
+
+        System.out.println("Welcome to the Doso\uD83D\uDC7E Market!");
+        System.out.println("Please type the option's NUMBER to navigate!");
+
+        for (int i = 0; i < landingPageOptions.length; i++) {
+            System.out.println(i+1 + "- " + landingPageOptions[i]);
+        }
+
+        int selectedOption = scanner.nextInt() - 1;
+        System.out.println(landingPageRoutes[selectedOption]);
     }
 }
