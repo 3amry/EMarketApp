4cd5a16b0606480fec7749c50d7041fb3b8434cb
3amry <74096502+3amry@users.noreply.github.com>
2025-06-13 06:26:43 +0300

    Extract landing page view.

---
+++ src/Views/LandingPage.java
@@ -0,0 +1,15 @@
+package Views;
+
+public class LandingPage {
+    public static String[] options = {
+            "Sign in",
+            "Sign up",
+            "Continue to market"
+    };
+    public static String[] routes = {
+            "Signing in...",
+            "Signing up...",
+            "Continuing to market..."
+    };
+
+}
